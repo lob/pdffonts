14493ef2a6cbac70e2ef32b35b3f64a9c592e2c8
Robin Joseph <robin@lob.com>
2016-05-14 11:04:33 -0700

    feat(fonts): add `fonts` funtion to list fonts

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # pdffonts
 
+[![Build Status](https://travis-ci.org/lob/pdffonts.svg?branch=master)](https://travis-ci.org/lob/pdffonts)
+[![Coverage Status](https://coveralls.io/repos/github/lob/pdffonts/badge.svg?branch=master)](https://coveralls.io/github/lob/pdffonts?branch=master)
+
 Node bindings for Poppler's `pdffonts` CLI.
 
 ## Dependencies
@@ -12,11 +15,40 @@ To install Poppler on Mac OS X using Homebrew:
 brew install poppler
 ```
 
-To install Poppler on Ubuntu:
+To install Poppler on Ubuntu 14.04:
 
 ```
 apt-get install pkg-config
 apt-get install libpoppler-cpp-dev
+```
+
+**Note: The version of Poppler on `apt-get` on Ubuntu 12.04 doesn't contain all the functions necessary for this package. If you're using 12.04, you need to build the latest version Poppler from source.**
+
+## Usage
+
+#### `PDFFonts.fonts()`
+
+```
+Returns an array of font objects.
+@param {String} path - path to the PDF
+@returns {Array<Object>} array of font objects
+```
+
+Here's an example font object:
+
+```js
+{
+  name: 'LDJWDV+DejaVuSerif-Bold',
+  type: 'CID TrueType',
+  encoding: 'Identity-H',
+  embedded: true,
+  subset: true,
+  unicode: true,
+  object: {
+    number: 8,
+    generation: 0
+  }
+}
 ```
 
 ## Testing
@@ -24,6 +56,34 @@ apt-get install libpoppler-cpp-dev
 ```bash
 $ npm i
 $ npm test
+```
+
+## Coverage
+
+The coverage report is generated using [`lcov`](http://ltp.sourceforge.net/coverage/lcov.php), so you need to make sure you have it installed:
+
+To install `lcov` on Mac OS X using Homebrew:
+
+```bash
+$ brew install lcov
+```
+
+To install `lcov` on Ubuntu 14.04:
+
+```bash
+$ apt-get install lcov
+```
+
+Once it's installed, you can generate a `coverage.info` file by running:
+
+```bash
+$ npm run cover
+```
+
+If you want to view it as an HTML file to see which lines haven't been covered, you can run the following to generate a `coverage/index.html`:
+
+```bash
+$ npm run cover:html
 ```
 
 ## Linting
--- test/pdffonts.test.js
+++ test/pdffonts.test.js
@@ -1,13 +1,75 @@
 'use strict';
 
-var Pdffonts = require('../lib/pdffonts');
+var Path = require('path');
+
+var PDFFonts = require('../lib/pdffonts');
+
+var EMBEDDED_FONTS_PATH    = Path.resolve(__dirname, 'assets/embedded-fonts.pdf');
+var NONEMBEDDED_FONTS_PATH = Path.resolve(__dirname, 'assets/nonembedded-fonts.pdf');
+var NO_FONTS_PATH          = Path.resolve(__dirname, 'assets/no-fonts.pdf');
+var NONEXISTENT_PATH       = Path.resolve(__dirname, 'assets/nonexistent.pdf');
+var NON_PDF_PATH           = Path.resolve(__dirname, 'assets/non-pdf.png');
 
 describe('pdffonts', function () {
 
-  describe('hello', function () {
+  describe('fonts', function () {
 
-    it('returns "world"', function () {
-      expect(Pdffonts.hello()).to.eql('world');
+    it('returns an array of font objects', function () {
+      var fonts = PDFFonts.fonts(EMBEDDED_FONTS_PATH);
+      expect(fonts).to.eql([{
+        name: 'LDJWDV+DejaVuSerif-Bold',
+        type: 'CID TrueType',
+        encoding: 'Identity-H',
+        embedded: true,
+        subset: true,
+        unicode: true,
+        object: {
+          number: 8,
+          generation: 0
+        }
+      }]);
+    });
+
+    it('detects nonembedded fonts', function () {
+      var fonts = PDFFonts.fonts(NONEMBEDDED_FONTS_PATH);
+      fonts.forEach(function (font) {
+        expect(font.embedded).to.be.false;
+      });
+    });
+
+    it('returns an empty array if there are no fonts', function () {
+      var fonts = PDFFonts.fonts(NO_FONTS_PATH);
+      expect(fonts).to.be.empty;
+    });
+
+    it('throws an error if a file name is not passed in', function (done) {
+      try {
+        PDFFonts.fonts();
+      } catch (err) {
+        expect(err).to.be.an.instanceof(Error);
+        expect(err.message).to.eql('file name is required');
+        done();
+      }
+    });
+
+    it('throws an error if a file does not exist', function (done) {
+      try {
+        PDFFonts.fonts(NONEXISTENT_PATH);
+      } catch (err) {
+        expect(err).to.be.an.instanceof(Error);
+        expect(err.message).to.eql('file is not a valid PDF');
+        done();
+      }
+    });
+
+    it('throws an error if the file is not a PDF', function (done) {
+      try {
+        PDFFonts.fonts(NON_PDF_PATH);
+      } catch (err) {
+        expect(err).to.be.an.instanceof(Error);
+        expect(err.message).to.eql('file is not a valid PDF');
+        done();
+      }
     });
 
   });
