c2abba2c00df837113e23c99e3930fbfd2652343
Adrian Falleiro <adrian.falleiro@lob.com>
2022-08-25 12:18:06 -0400

    Fix indenting

--- test/pdffonts.test.js
+++ test/pdffonts.test.js
@@ -19,165 +19,165 @@ describe('pdffonts', () => {
 
     it('returns an array of font objects', () => {
       return PDFFonts.fonts(EMBEDDED_FONTS_PATH)
-      .then((fonts) => {
-        expect(fonts).to.eql([{
-          name: 'LDJWDV+DejaVuSerif-Bold',
-          type: 'CID TrueType',
-          encoding: 'Identity-H',
-          embedded: true,
-          subset: true,
-          unicode: true,
-          object: {
-            number: 8,
-            generation: 0
-          }
-        }]);
-      });
+        .then((fonts) => {
+          expect(fonts).to.eql([{
+            name: 'LDJWDV+DejaVuSerif-Bold',
+            type: 'CID TrueType',
+            encoding: 'Identity-H',
+            embedded: true,
+            subset: true,
+            unicode: true,
+            object: {
+              number: 8,
+              generation: 0
+            }
+          }]);
+        });
     });
 
     it('detects nonembedded fonts', () => {
       return PDFFonts.fonts(NONEMBEDDED_FONTS_PATH)
-      .then((fonts) => {
-        fonts.forEach((font) => {
-          expect(font.embedded).to.be.false;
+        .then((fonts) => {
+          fonts.forEach((font) => {
+            expect(font.embedded).to.be.false;
+          });
         });
-      });
     });
 
     it('returns an empty array if there are no fonts', () => {
       return PDFFonts.fonts(NO_FONTS_PATH)
-      .then((fonts) => {
-        expect(fonts).to.be.empty;
-      });
+        .then((fonts) => {
+          expect(fonts).to.be.empty;
+        });
     });
 
     it('handles Type 3 fonts', () => {
       return PDFFonts.fonts(TYPE_3_FONT_PATH)
-      .then((fonts) => {
-        expect(fonts).to.eql([{
-          name: null,
-          type: 'Type 3',
-          encoding: 'Custom',
-          embedded: true,
-          subset: false,
-          unicode: false,
-          object: {
-            number: 4,
-            generation: 0
-          }
-        }]);
-      });
+        .then((fonts) => {
+          expect(fonts).to.eql([{
+            name: null,
+            type: 'Type 3',
+            encoding: 'Custom',
+            embedded: true,
+            subset: false,
+            unicode: false,
+            object: {
+              number: 4,
+              generation: 0
+            }
+          }]);
+        });
     });
 
     it('handles fonts with invalid object ID', () => {
       return PDFFonts.fonts(INVALID_OBJECT_ID_PATH)
-      .then(([font]) => {
-        expect(font.object).to.be.null;
-      });
+        .then(([font]) => {
+          expect(font.object).to.be.null;
+        });
     });
 
     it('handles language pack-required mappings', () => {
       return PDFFonts.fonts(LANGUAGE_PACK_REQUIRED)
-      .then((fonts) => {
-        expect(fonts).to.eql([
-          {
-            name: 'DotumChe',
-            type: 'CID Type 0',
-            encoding: 'KSCms-UHC-H',
-            embedded: false,
-            subset: false,
-            unicode: false,
-            object: {
-              number: 41,
-              generation: 0
+        .then((fonts) => {
+          expect(fonts).to.eql([
+            {
+              name: 'DotumChe',
+              type: 'CID Type 0',
+              encoding: 'KSCms-UHC-H',
+              embedded: false,
+              subset: false,
+              unicode: false,
+              object: {
+                number: 41,
+                generation: 0
+              }
+            }, {
+              name: 'SimSun',
+              type: 'CID Type 0',
+              encoding: 'GBK-EUC-H',
+              embedded: false,
+              subset: false,
+              unicode: false,
+              object: {
+                number: 37,
+                generation: 0
+              }
+            }, {
+              name: 'MS-Gothic',
+              type: 'CID Type 0',
+              encoding: '90ms-RKSJ-H',
+              embedded: false,
+              subset: false,
+              unicode: false,
+              object: {
+                number: 44,
+                generation: 0
+              }
+            }, {
+              name: 'MingLiU',
+              type: 'CID Type 0',
+              encoding: 'ETen-B5-H',
+              embedded: false,
+              subset: false,
+              unicode: false,
+              object: {
+                number: 43,
+                generation: 0
+              }
+            }, {
+              name: 'Helvetica',
+              type: 'Type 1',
+              encoding: 'Custom',
+              embedded: false,
+              subset: false,
+              unicode: false,
+              object: {
+                number: 39,
+                generation: 0
+              }
+            }, {
+              name: 'Helvetica',
+              type: 'Type 1',
+              encoding: 'Custom',
+              embedded: false,
+              subset: false,
+              unicode: false,
+              object: {
+                number: 38,
+                generation: 0
+              }
+            }, {
+              name: 'Helvetica',
+              type: 'Type 1',
+              encoding: 'Custom',
+              embedded: false,
+              subset: false,
+              unicode: false,
+              object: {
+                number: 40,
+                generation: 0
+              }
             }
-          }, {
-            name: 'SimSun',
-            type: 'CID Type 0',
-            encoding: 'GBK-EUC-H',
-            embedded: false,
-            subset: false,
-            unicode: false,
-            object: {
-              number: 37,
-              generation: 0
-            }
-          }, {
-            name: 'MS-Gothic',
-            type: 'CID Type 0',
-            encoding: '90ms-RKSJ-H',
-            embedded: false,
-            subset: false,
-            unicode: false,
-            object: {
-              number: 44,
-              generation: 0
-            }
-          }, {
-            name: 'MingLiU',
-            type: 'CID Type 0',
-            encoding: 'ETen-B5-H',
-            embedded: false,
-            subset: false,
-            unicode: false,
-            object: {
-              number: 43,
-              generation: 0
-            }
-          }, {
-            name: 'Helvetica',
-            type: 'Type 1',
-            encoding: 'Custom',
-            embedded: false,
-            subset: false,
-            unicode: false,
-            object: {
-              number: 39,
-              generation: 0
-            }
-          }, {
-            name: 'Helvetica',
-            type: 'Type 1',
-            encoding: 'Custom',
-            embedded: false,
-            subset: false,
-            unicode: false,
-            object: {
-              number: 38,
-              generation: 0
-            }
-          }, {
-            name: 'Helvetica',
-            type: 'Type 1',
-            encoding: 'Custom',
-            embedded: false,
-            subset: false,
-            unicode: false,
-            object: {
-              number: 40,
-              generation: 0
-            }
-          }
-        ]);
-      });
+          ]);
+        });
     });
 
     it('throws an error if a file name is not passed in', () => {
       return PDFFonts.fonts()
-      .catch((err) => err)
-      .then((err) => {
-        expect(err).to.be.an.instanceof(Error);
-        expect(err.message).to.eql('expected 2 arguments');
-      });
+        .catch((err) => err)
+        .then((err) => {
+          expect(err).to.be.an.instanceof(Error);
+          expect(err.message).to.eql('expected 2 arguments');
+        });
     });
 
     it('throws an error if first argument is not a string', () => {
       return PDFFonts.fonts(42)
-      .catch((err) => err)
-      .then((err) => {
-        expect(err).to.be.an.instanceof(Error);
-        expect(err.message).to.eql('expected arg 0: string filename');
-      });
+        .catch((err) => err)
+        .then((err) => {
+          expect(err).to.be.an.instanceof(Error);
+          expect(err.message).to.eql('expected arg 0: string filename');
+        });
     });
 
     it('throws an error if second argument is not a function', () => {
@@ -196,20 +196,20 @@ describe('pdffonts', () => {
 
     it('throws an error if a file does not exist', () => {
       return PDFFonts.fonts(NONEXISTENT_PATH)
-      .catch((err) => err)
-      .then((err) => {
-        expect(err).to.be.an.instanceof(Error);
-        expect(err.message).to.eql('file is not a valid PDF');
-      });
+        .catch((err) => err)
+        .then((err) => {
+          expect(err).to.be.an.instanceof(Error);
+          expect(err.message).to.eql('file is not a valid PDF');
+        });
     });
 
     it('throws an error if the file is not a PDF', () => {
       return PDFFonts.fonts(NON_PDF_PATH)
-      .catch((err) => err)
-      .then((err) => {
-        expect(err).to.be.an.instanceof(Error);
-        expect(err.message).to.eql('file is not a valid PDF');
-      });
+        .catch((err) => err)
+        .then((err) => {
+          expect(err).to.be.an.instanceof(Error);
+          expect(err.message).to.eql('file is not a valid PDF');
+        });
     });
 
   });
