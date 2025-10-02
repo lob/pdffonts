0064252e9e80749ba5c4d1d6996ca2c063cbc75d
Michael Morgan <94082162+mike-r-morgan@users.noreply.github.com>
2025-10-02 14:44:25 -0700

    Chore: resolve high vulnerabilities, upgrade eslint (#38)

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'script',
+      globals: {
+        // From eslint-config-lob env settings
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        global: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        // Mocha globals
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly'
+      }
+    },
+    rules: {
+      // From eslint-config-lob rules
+      'no-class-assign': 'error',
+      'no-confusing-arrow': 'error',
+      'no-const-assign': 'error',
+      'no-constant-condition': 'error',
+      'no-dupe-class-members': 'error',
+      'no-this-before-super': 'error',
+      'no-var': 'error',
+      'object-shorthand': ['error', 'properties'],
+      'prefer-arrow-callback': 'error',
+      'prefer-const': 'error',
+      'prefer-spread': 'error',
+      'prefer-template': 'error',
+      'require-yield': 'error'
+    }
+  }
+];
